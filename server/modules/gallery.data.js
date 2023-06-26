4f784821e9abe1ed61526f95241e8f858e305d8b
Javier Navarro <javierna182@gmail.com>
2023-06-25 22:19:08 -0500

    added pictures and updated the galleyItems

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/javifoto1 .jpeg', description: 'Me and my daughter.', likes: 0 },
+    { id: 2, path: 'images/javifoto2 .jpeg', description: 'Me and my wife.', likes: 0 },
+    { id: 3, path: 'images/javifoto3 .jpeg', description: 'Me, my wife and brothers.', likes: 0 },
+    { id: 4, path: 'images/javifoto4 .jpeg', description: 'Me and my father in law.', likes: 0 },
+    { id: 5, path: 'images/javifoto5 .jpeg', description: 'Me, my daughter and our dogs.', likes: 0 },
+    { id: 6, path: 'images/javifoto6 .jpeg', description: 'Me, my wife, my dad and brothers.', likes: 0 }
 ];
 
 module.exports = galleryItems;
